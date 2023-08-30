099ac1efe35d5494c8ee99def2c19c439c12128b
new git project <ld4130@kakao.com>
2023-08-30 18:00:50 +0900

    요청 매핑

---
+++ src/main/java/hello/springmvc/requestmapping/MappingController.java
@@ -0,0 +1,58 @@
+package hello.springmvc.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MappingController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/hello-basic")
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    /**
+     * method 특정 HTTP 메서드 요청만 허용
+     * GET, HEAD, POST, PUT, PATCH, DELETE
+     */
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1(){
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
+    /**
+     * 축약 어노테이션
+     * @GetMapping, @PostMapping, @PutMapping, @DeleteMapping, @PatchMapping
+     */
+    @GetMapping("mapping-get-v2")
+    public String mappingGetV2(){
+        log.info("mappingGetV2");
+        return "OK";
+    }
+
+    /**
+     * PathVariable 사용
+     * 변수명이 같으면 생략 가능
+     * @Pathvariable("userId") String userId -> @PathVariable userId
+     */
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data){
+        log.info("mappingPath userId={}", data);
+        return "OK";
+    }
+
+    /**
+     * PathVariable 다중 사용
+     */
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "OK";
+    }
+
+}
