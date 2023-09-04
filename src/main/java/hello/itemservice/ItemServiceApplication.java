03c4bcac979d07bb02e2c864d9ed0a8a082943d0
new git project <ld4130@naver.com>
2023-09-05 01:13:07 +0900

    메시지, 국제화 - 스프링 메시지 소스 설정

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -10,4 +10,18 @@ public class ItemServiceApplication {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+	/**
+	 * 스프링 부트 사용 - MessageSource 를 자동으로 스프링 빈으로 등록.
+	 *
+	   @Bean
+	   public MessageSource messageSource() {
+	 	   ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+
+	       messageSource.setBasenames("messages", "errors");
+	 	   messageSource.setDefaultEncoding("utf-8");
+
+	       return messageSource;
+	   }
+	 */
+
 }
