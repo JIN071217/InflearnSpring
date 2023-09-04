6be018d0e8b4ec884f270e1b8cd5fe0c463a67b6
new git project <ld4130@naver.com>
2023-09-04 22:16:31 +0900

    editForm.html - 동적 html(thymeleaf)
    BasicItemController.java
    * 상품 수정
    
    editForm - 수정 폼 뷰
    
    edit - 상품수정 처리
    1. Redirect 상품 수정 템플릿 호출 대신, 상품 상세 화면으로 이동
    
    * PRG - Post Redirect Get
    addItemV4 - 리턴 "basic/item" 할 경우 새로고침 시 POST 데이터 쌓이는 문제 발생
    
    addItemV5 - redirect 적용, 뷰 템플릿 이동하는게 아니라, 상세 화면으로 리다이렉트
    1. 마지막 호출한 내용 GET 으로 호출, 데이터 쌓이는 문제 해결
    
    addItemV6 - redirect:/basic/item/ + item.getId() URL 인코딩 문제
    1. RedirectAttribute 사용 - URL 인코딩, pathVariable 쿼리 파라미터 처리

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -90,12 +91,42 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item) {
 
         itemRepository.save(item);
 
         return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item) {
+
+        itemRepository.save(item);
+
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+
+        Item saveItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", saveItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, Model model, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
     }
 
     /**
