b1861c5417effe051f4f51f2ad8212e18f848404
new git project <ld4130@naver.com>
2023-09-09 18:43:52 +0900

    ObjectError, FieldError 생성자 - 오류 메시지 처리 : 관리 전략 *errors.properties - 구체적인 것에서 덜 구체적인 메시지 레벨 별 오류 메시지 출력

---
+++ src/test/java/hello/itemservice/vlidation/MessageCodesResolverTest.java
@@ -0,0 +1,32 @@
+package hello.itemservice.vlidation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.ObjectError;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MessageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject() {
+        String[] messagesCodes = codesResolver.resolveMessageCodes("required", "item");
+        assertThat(messagesCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+
+        assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+    }
+}
