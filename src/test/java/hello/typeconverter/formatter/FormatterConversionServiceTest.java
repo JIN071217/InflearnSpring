c197719851c6c00665df7b100f5a53033cb9a9f7
new git project <ld4130@naver.com>
2023-09-27 22:51:49 +0900

    스프링 타입 컨버터 - Formatter 지원하는 Conversion Service
    * DefaultFormattingConversionService 상속 관계
      1. FormatterConversionService > ConversionService 기능 상속
          - 결과적으로 컨버터도 포맷터도 모두 등록할 수 있다.
          - ConversionService 가 제공하는  convert 를 사용
    
    * 추가
      1. 스프링 부트는 DefaultFormattingConversionService 는 상속 받은 WebConversionService 를 내부에서 사용

---
+++ src/test/java/hello/typeconverter/formatter/FormatterConversionServiceTest.java
@@ -0,0 +1,38 @@
+package hello.typeconverter.formatter;
+
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringIpPortConverter;
+import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.format.support.DefaultFormattingConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class FormatterConversionServiceTest {
+
+    @Test
+    void formattingConversionService() {
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+
+        //컨버터 등록
+        conversionService.addConverter(new StringIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        //포맷터 등록
+        conversionService.addFormatter(new MyNumberFormatter());
+
+        //컨버터 사용
+        IpPort ipPort = conversionService.convert("127.0.0.1:8090", IpPort.class);
+        assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8090));
+
+        //포맷터 사용
+        String convert = conversionService.convert(1000, String.class);
+        assertThat(convert).isEqualTo("1,000");
+
+        Object convert2 = conversionService.convert("1,000", Long.class);
+        assertThat(convert2).isEqualTo(1000L);
+
+    }
+}
